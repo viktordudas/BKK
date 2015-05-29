6e2287ccb5802dfa7af9d79f376e2409cf63327e
viktordudas <viktordudas@hotmail.com>
2015-05-29 06:55:36 +0200

    design fix

--- routes/index.js
+++ routes/index.js
@@ -17,20 +17,20 @@ var sendCB = function(tx_err, state) {
 router.get('/', function(req, res, next) {
   console.log(req.query.Vonal);
 
+  if (req.Query.Vonal != "") {
 // var uri = 'amqps://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost;
-  var uri = 'amqps://' + encodeURIComponent('RootManageSharedAccessKey') + ':' + encodeURIComponent('mMvTQ0E8ZC879TMt1iZ9mTDQsC+/r9gYYHWHZ+VRuTU=') + '@' + 'bkksbn.servicebus.windows.net';
+    var uri = 'amqps://' + encodeURIComponent('RootManageSharedAccessKey') + ':' + encodeURIComponent('mMvTQ0E8ZC879TMt1iZ9mTDQsC+/r9gYYHWHZ+VRuTU=') + '@' + 'bkksbn.servicebus.windows.net';
 
+    var sendAddr = 'bkkhub';
 
-  var sendAddr = 'bkkhub';
-
-  var client = new AMQPClient(AMQPClient.policies.EventHubPolicy);
-  client.connect(uri, function() {
-    client.send({
-      "Vonal": req.query.Vonal,
-      "EntryTime": new Date()
-    }, sendAddr, {'x-opt-partition-key': 'pk' + 0}, sendCB);
-  });
-
+    var client = new AMQPClient(AMQPClient.policies.EventHubPolicy);
+    client.connect(uri, function () {
+      client.send({
+        "Vonal": req.query.Vonal,
+        "EntryTime": new Date()
+      }, sendAddr, {'x-opt-partition-key': 'pk' + 0}, sendCB);
+    });
+  }
   res.render('index', { title: 'Express' });
 });
 
